bd5f4ddd8bcf9c1b90720de1824a9f25ccc76d0f
Foyo89 <grzegorzfoik@gmail.com>
2017-07-19 14:43:13 +0200

    Servlety

---
+++ Lesson12/src/main/java/pl/sda/web/jsp/DbServlet.java
@@ -0,0 +1,138 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.sda.web.jsp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Adrian
+ */
+@WebServlet(name = "DbServlet", urlPatterns = {"/DbServlet"})
+public class DbServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet DbServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            //out.println("<form action='/servlet/DbServlet' method='post'><input type='text'/><input type='submit' value='Ok'/></form>");
+            
+            out.println("<form action=\"/Lesson12/DbServlet\" method=\"post\">");
+            out.println("First name:<br>");
+            out.println("<input type=\"text\" name=\"firstname\"><br>");
+            out.println("Last name:<br>");
+            out.println("<input type=\"text\" name=\"lastname\"></p>");
+            out.println("<input type=\"submit\" value=\"Submit\">");
+            out.println("</form>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        ServletContext ctx = getServletContext();
+
+        //initialize DB Connection
+        String dbURL = ctx.getInitParameter("jdbc:mysql://localhost/books_db?serverTimezone=CET&useSSL=false");
+        String user = ctx.getInitParameter("root");
+        String pwd = ctx.getInitParameter("root");
+        
+        try {
+            DBConnectionManager connectionManager = new DBConnectionManager("jdbc:mysql://localhost/books_db?serverTimezone=CET&useSSL=false", "root", "root");
+            ctx.setAttribute("DBConnection", connectionManager.getConnection());
+            Connection con = (Connection) getServletContext().getAttribute("DBConnection");
+            java.sql.PreparedStatement ps = null;
+            try {
+                //ps = con.prepareStatement("insert into users(first_name, last_name) values (?,?)");
+                ps = con.prepareStatement("insert into users(first_name, last_name) values ('raz','dwa')");
+//                ps.setString(1, request.getParameter("firstname"));
+//                ps.setString(2, request.getParameter("lastname"));    
+                ps.execute();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            try (PrintWriter out = response.getWriter()) {
+                out.println("<!DOCTYPE html>");
+                out.println("<html>");
+                out.println("<head>");
+                out.println("<title>Servlet DbServlet</title>");
+                out.println("</head>");
+                out.println("<body>");
+                out.println("<h4><a href='/servlet/DbServlet'>Powrót</a></h4>");
+                out.println("</body>");
+                out.println("</html>");
+            }
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DbServlet.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(DbServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ Lesson12/src/main/java/pl/sda/web/jsp/MyServlet.java
@@ -0,0 +1,125 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.sda.web.jsp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Grzegorz
+ */
+public class MyServlet extends HttpServlet {
+    
+    MyFilter myFilter = new MyFilter();
+    
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+//            if (request.getMethod().equals("GET"))
+//                response.sendRedirect("/Lesson12/");
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet MyServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet MyServlet at " + request.getContextPath() + "</h1>");
+            out.println("<h3>wersja aplikacji: " + getServletContext().getInitParameter("version") + "</h3>");
+            if (request.getParameter("param1") != null && request.getParameter("param2") != null)
+            out.println("<p>param1 = "+request.getParameter("param1")+" param2 = "+request.getParameter("param2")+"</p>");
+            out.println("<a href='/Lesson12/MyServlet?param1=33&param2=44'>servlet link</a></p>");
+            
+            out.println("<form action=\"/Lesson12/MyServlet\" method=\"post\">");
+            out.println("First name:<br>");
+            out.println("<input type=\"text\" name=\"firstname\"><br>");
+            out.println("Last name:<br>");
+            out.println("<input type=\"text\" name=\"lastname\"></p>");
+            out.println("<input type=\"submit\" value=\"Submit\">");
+            out.println("</form>");
+            
+            out.println("</p>");
+            
+            if(request.getParameter("firstname") != null && request.getParameter("lastname") != null)
+                out.println("<h2>Witaj "+request.getParameter("firstname")+" "+request.getParameter("lastname")+"</h2>");
+            
+            out.println("</p>");
+            
+            HttpSession session = request.getSession();
+            if (session.isNew()) {
+                session.setAttribute("licznik", 1);               
+            }else {
+                Integer counter = Integer.valueOf(session.getAttribute("licznik").toString());
+                session.setAttribute("licznik", ++counter);
+            }
+            out.println("<h5>Licznik: "+session.getAttribute("licznik")+"</h5>");
+            
+            //out.println("<p>"+request."</p>");
+            
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        processRequest(request, response);
+        
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
